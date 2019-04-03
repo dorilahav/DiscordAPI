1716f848adfaf401bf59c91b9ae01bc2340be8db
Dori Lahav Waisberg <dori.lahav@gmail.com>
2019-04-03 09:45:48 +0300

    Added LocalDateTime Serializer and Deserializer to JacksonModule.

--- src/main/java/com/dorilahav/api/jackson/JacksonModule.java
+++ src/main/java/com/dorilahav/api/jackson/JacksonModule.java
@@ -1,11 +1,14 @@
 package com.dorilahav.api.jackson;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 import com.dorilahav.api.jackson.deserializers.InstantDeserializer;
+import com.dorilahav.api.jackson.deserializers.LocalDateTimeDeserializer;
 import com.dorilahav.api.jackson.deserializers.OffsetDateTimeDeserializer;
 import com.dorilahav.api.jackson.serializers.InstantSerializer;
+import com.dorilahav.api.jackson.serializers.LocalDateTimeSerializer;
 import com.dorilahav.api.jackson.serializers.OffsetDateTimeSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -19,6 +22,9 @@ public class JacksonModule extends SimpleModule {
 		
 		addDeserializer(OffsetDateTime.class, new OffsetDateTimeDeserializer());
 		addSerializer(new OffsetDateTimeSerializer());
+		
+		addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+		addSerializer(new LocalDateTimeSerializer());
 	}
 
 }
